322be9a6a4afc53062c0049bffd4fe45f0adc4b5
htam00 <anonimozurik@gmail.com>
2019-11-26 13:04:39 -0300

    remove packages: morgan, path

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express')
-const path = require('path')
 const layouts = require('express-ejs-layouts')
-const logger = require('morgan')
 const bodyP = require('body-parser')
 const app = express()
 
